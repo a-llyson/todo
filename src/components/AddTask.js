3a9c8365f7b8c6bf7890dc80565ca1fe940ae824
a-llyson <67112226+a-llyson@users.noreply.github.com>
2021-05-02 22:41:53 -0400

    Updated README

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,29 @@
-const AddTask = () => {
+import { useState } from 'react'
+
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Todo</label>
-                <input type='text' placeholder='Add Todo'/>
+                <input type='text' placeholder='Add Todo' value={text} onChange={(e) => setText(e.target.value)} 
+                />
             </div>
             <div className='form-control'>
                 <label>Day & Time</label>
-                <input type='text' placeholder='Add Day & Time'/>
+                <input type='text' placeholder='Add Day & Time' value={day} onChange={(e) => setDay(e.target.value)} 
+                />
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='checkbox' value={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}  />
             </div>
 
             <input type='submit' value='Save Task' className='btn btn-block' />
